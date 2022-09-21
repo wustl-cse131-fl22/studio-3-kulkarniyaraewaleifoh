6fad5b8631b0a7ab2196fca998bca5f4051b3b83
amritakulkarni <amritakulkarni@localhost>
2022-09-21 17:21:09 -0500

    Studio 3!

---
+++ src/studio3/Seive.java
@@ -0,0 +1,28 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Seive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+	    System.out.println("How many numbers would you like to use?");
+	    int n = in.nextInt();
+	    
+	    boolean[] primeNumbers = new boolean[n + 1];
+	    
+	    for(int i=0; i < n; i++) {
+	    	primeNumbers[i]= true;
+	    
+	    	
+	    	
+	    	
+	    	
+	    	System.out.println(i);
+	    }
+		
+	}
+
+}
